ec54dc5ee531b965176ae8c67c1e62c5d6445fe5
mohamed fawzzi <mohamedfawzzimohamed@gmail.com>
2025-03-02 22:20:30 +0200

    upload

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:2000',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     },
     withCredentials: true
 });
 
-export default api 
+export default api;
